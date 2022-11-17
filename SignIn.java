c440cb44ded102e9c07f459bb8eff2cdf71fdc9c
BarirNada00 <116973510+BarirNada00@users.noreply.github.com>
2022-11-17 22:15:05 +0100

    Add files via upload

---
+++ SignIn.java
@@ -0,0 +1,15 @@
+package com.example.bdchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        getSupportActionBar().hide();
+    }
+}
